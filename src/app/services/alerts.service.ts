8471b9c396dfe30f7fc8dc9e146233b74c70971f
azizkale <azizkale@hotmail.com>
2023-03-15 14:45:22 +0100

    fix/alert

--- src/app/services/alerts.service.ts
+++ src/app/services/alerts.service.ts
@@ -8,12 +8,12 @@ export class AlertsService {
   constructor() { }
   alert(text: string, classForColor: string, parentElement: HTMLElement) {
     const alertTemplate = `
-    <div #alert
+    <div #alert id="alert"
     class="alert ${classForColor} alert-dismissible fade show"
     role="alert"
   >
     <strong>${text}</strong>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"   onclick="document.getElementById('alert').style.display='none'"></button>
 
   `;
     parentElement.innerHTML = alertTemplate;
