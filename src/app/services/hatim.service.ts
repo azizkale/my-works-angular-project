296cd5d22bc3b40cfc0beadf3a27276f62f27f70
azizkale <azizkale@hotmail.com>
2023-03-29 16:03:01 +0200

    environment object changed

--- src/app/services/hatim.service.ts
+++ src/app/services/hatim.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AuthGuard } from '../Auth.Guard';
 import { EMPTY, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Router } from '@angular/router';
 
 @Injectable({
