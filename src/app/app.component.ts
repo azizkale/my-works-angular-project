867a5f72cd833e10f0826f034c2e993a53751cd0
azizkale <azizkale@hotmail.com>
2023-03-29 17:07:05 +0200

    logo-added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  displayName: string | any = localStorage.getItem('displayName');
 
-
-  w3_open(mySidebar: any, myOverlay: any) {
-    if (mySidebar.style.display === "block") {
-      mySidebar.style.display = "none";
-      myOverlay.style.display = "none";
-    } else {
-      mySidebar.style.display = "block";
-      myOverlay.style.display = "block";
-    }
-  }
-  // Close the sidebar with the close button
-  w3_close(mySidebar: HTMLElement, myOverlay: HTMLElement) {
-    mySidebar.style.display = "none";
-    myOverlay.style.display = "none";
-  }
 
 }
