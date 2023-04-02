6e3da53bf64c11f49bc71f8d022d81d0f5b2c6e6
azizkale <azizkale@hotmail.com>
2023-04-02 16:25:40 +0200

    fix/default profile photo

--- src/app/left-column/left-column.component.ts
+++ src/app/left-column/left-column.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftColumnComponent implements OnInit {
   displayName: string | any = localStorage.getItem('displayName');
-  photoURL: string | any = localStorage.getItem('photoURL')
+  photoURL: string | any;
 
   ngOnInit(): void {
+    this.photoURL = (localStorage.getItem('photoURL') !== undefined ? 'https://www.w3schools.com/w3css/img_avatar3.png' : localStorage.getItem('photoURL'))
   }
 
 }
