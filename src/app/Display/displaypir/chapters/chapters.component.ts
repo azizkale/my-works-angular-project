66fe5aa6b7279764db6a06527a4ea8c67905f6f8
azizkale <azizkale@hotmail.com>
2023-04-24 20:55:48 +0200

    ..

--- src/app/Display/displaypir/chapters/chapters.component.ts
+++ src/app/Display/displaypir/chapters/chapters.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { DisplaypirService } from 'src/app/services/displaypir.service';
+import { Chapter } from 'src/models/Chapter';
+import { Pir } from 'src/models/Pir';
 
 @Component({
   selector: 'app-chapters',
@@ -11,11 +13,14 @@ import { DisplaypirService } from 'src/app/services/displaypir.service';
 export class ChaptersComponent implements OnInit {
   retrieveChapterForm: FormGroup;
   selectedPirId: any;
+  pir: Pir
+  chapters: any[]
 
   constructor(
     public fb: FormBuilder,
     private activeroute: ActivatedRoute,
-    private displaypirservice: DisplaypirService
+    private displaypirservice: DisplaypirService,
+    private renderer: Renderer2
   ) { }
 
   async ngOnInit() {
@@ -38,9 +43,16 @@ export class ChaptersComponent implements OnInit {
 
   retrieveChaptersByPirId() {
     this.displaypirservice.retrieveChaptersByPirId(this.selectedPirId).subscribe({
-      next: (ress) => {
-        console.log(ress)
+      next: (pir: Pir) => {
+        console.log(pir)
+        this.chapters = Object.values(pir.chapters)
       }
     })
+  }
+
+  appendContentToTag(htmlElement: HTMLElement, chapter: Chapter) {
+    const p: HTMLParagraphElement = this.renderer.createElement('b');
+    p.innerHTML = chapter.chapterName
+    this.renderer.appendChild(htmlElement, p)
   }
 }
