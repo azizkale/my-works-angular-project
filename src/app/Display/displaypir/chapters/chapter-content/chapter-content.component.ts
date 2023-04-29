8ae6e34a56a1102e3c05cb2ae43df66f0f63d7c3
azizkale <azizkale@hotmail.com>
2023-04-29 23:22:24 +0200

    Feat/angular snackbar

--- src/app/Display/displaypir/chapters/chapter-content/chapter-content.component.ts
+++ src/app/Display/displaypir/chapters/chapter-content/chapter-content.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { AlertsService } from 'src/app/services/alerts.service';
 import { DisplaypirService } from 'src/app/services/displaypir.service';
 import { Chapter } from 'src/models/Chapter';
 import { WordPair } from 'src/models/WordPair';
@@ -20,7 +22,9 @@ export class ChapterContentComponent implements OnInit {
   constructor(
     public fb: FormBuilder,
     private activeroute: ActivatedRoute,
-    private displaypirservice: DisplaypirService
+    private displaypirservice: DisplaypirService,
+    private alertservice: AlertsService,
+    private snackbar: MatSnackBar
   ) {
     this.formChapterRetrieve()
 
@@ -57,8 +61,13 @@ export class ChapterContentComponent implements OnInit {
         Object.values(this.chapterContent.nativeElement.getElementsByTagName('b')).map((el: HTMLElement | any) => {
           el.addEventListener('mouseover', async () => {
             // getting meaning from wordPairs
-            const wrd = Object.values(this.selectedChapter.wordPairs).find((pair: WordPair) => pair.word.trim() === el.innerHTML.trim())
-            console.log(wrd)
+            const word_: WordPair | any = Object.values(this.selectedChapter.wordPairs).find((pair: WordPair) => pair.word.trim() === el.innerHTML.trim())
+
+            this.snackbar.open(word_.meaning, 'X', {
+              duration: 2 * 1000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top',
+            });
           });
         })
 
