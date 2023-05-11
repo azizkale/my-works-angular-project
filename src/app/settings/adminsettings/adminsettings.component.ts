29a89592d59782791cfdbe4cfbed2b24ac3fa7db
azizkale <azizkale@hotmail.com>
2023-05-11 17:32:40 +0200

    .

--- src/app/settings/adminsettings/adminsettings.component.ts
+++ src/app/settings/adminsettings/adminsettings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { GroupService } from 'src/app/services/group.service';
+import { Group } from 'src/models/Group';
 
 @Component({
   selector: 'adminsettings',
@@ -9,36 +10,73 @@ import { GroupService } from 'src/app/services/group.service';
 })
 export class AdminsettingsComponent implements OnInit {
   createGroupForm: FormGroup
+  retrieveGroupsForm: FormGroup
+  updateGroupForm: FormGroup
+  groups: Group[];
+
   constructor(
     private fb: FormBuilder,
     private groupservice: GroupService
   ) { }
 
   ngOnInit(): void {
-    this.createCreateNewGroupForm();
     this.retrieveGroups()
+    this.createCreateNewGroupForm();
+    this.createReteiveGroupsForm();
+    this.createUpdateGroupForm();
   }
 
+  // forms=====================
   createCreateNewGroupForm() {
+    this.retrieveGroupsForm = this.fb.group({
+      groupName: ['', Validators.required],
+      mentorEmail: ['', Validators.required],
+    });
+  }
+  createReteiveGroupsForm() {
     this.createGroupForm = this.fb.group({
+      groupId: ['', Validators.required],
+      groupName: ['', Validators.required],
+      mentorEmail: ['', Validators.required],
+    });
+  }
+  createUpdateGroupForm() {
+    this.updateGroupForm = this.fb.group({
+      groupId: ['', Validators.required],
+      mentorId: ['', Validators.required],
       groupName: ['', Validators.required],
       mentorEmail: ['', Validators.required],
     });
   }
 
+  // functions to Groups=================
   createGroup(groupName: any, mentorEmail: any) {
     this.groupservice.createGroup(groupName, mentorEmail).subscribe({
       next: (result) => {
-        console.timeLog(result)
+        console.log(result)
+        this.retrieveGroups()
       }
     })
   }
 
   retrieveGroups() {
+    this.groups = []
     this.groupservice.retrieveGroups().subscribe({
       next: (result) => {
-        console.log(result)
+        this.groups = Object.values(result)
+        this.groups.forEach((group: Group) => {
+          this.retrieveGroupsForm.addControl(group.groupName, new FormControl(group.groupName));
+        });
       }
     })
+  }
+
+  updateGroup() {
+    console.log(this.updateGroupForm.value)
+  }
+
+  // functions to Users=================
+  retrieveSingleUser() {
+
   }
 }
